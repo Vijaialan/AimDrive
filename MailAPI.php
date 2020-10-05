31215cf447b38efcd9f79061ae12101d750aa903
VIJAI <vijay@stepnstones.in>
2020-10-05 17:12:34 +0530

    mail12

--- MailAPI.php
+++ MailAPI.php
@@ -30,7 +30,7 @@ $email->addContent(
 
 //$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
 
-$sendgrid = $SENDGRID_API_KEY;
+$sendgrid = new \SendGrid($SENDGRID_API_KEY);
 
 try {
     $response = $sendgrid->send($email);

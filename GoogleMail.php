52148d8df1a5a43be8e5361599545a1b23f49e2a
VIJAI <vijay@stepnstones.in>
2020-10-05 16:47:09 +0530

    mail

--- GoogleMail.php
+++ GoogleMail.php
@@ -12,3 +12,14 @@ mail($to, $title, $content, $titles);
 ?>
 
 
+
+<?php
+    ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
+    $from = "vija@stepnstones.in";
+    $to = "vija@stepnstones.in";
+    $subject = "PHP Mail Test script";
+    $message = "This is a test to check the PHP Mail functionality";
+    $headers = "From:" . $from;
+$headers .= "Reply-To: $to";
+    var_dump(mail($to,$subject,$message,$headers));

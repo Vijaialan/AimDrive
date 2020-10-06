c0d7f7168a6095db805a044e76c6d957330e1ada
VIJAI <vijay@stepnstones.in>
2020-10-06 23:55:49 +0530

    mailler12

--- MailAPI.php
+++ MailAPI.php
@@ -1,25 +1,18 @@
 <?php
 //require 'vendor/autoload.php'; // If you're using Composer (recommended)
 // Comment out the above line if not using Composer
-
-require 'vendor/autoload.php';
+//require 'vendor/autoload.php';
 require "sendgrid/sendgrid-php.php";
 
- //require_once 'MailApiKey.php';
+// require_once 'MailApiKey.php';
 // If not using Composer, uncomment the above line and
 // download sendgrid-php.zip from the latest release here,
 // replacing <PATH TO> with the path to the sendgrid-php.php file,
 // which is included in the download:
 // https://github.com/sendgrid/sendgrid-php/releases
-
-
-require_once 'dbconfig.php';
+//require_once 'dbconfig.php';
 
 require_once '/var/secure/MailApiKey.php';
-
-echo $Sendgrid;
-
-
 
 $email = new \SendGrid\Mail\Mail(); 
 $email->setFrom("vijay@stepnstones.in", "Example User");
@@ -30,8 +23,6 @@ $email->addContent(
     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
 );
 
-//$sendgrid = new \SendGrid("'".$SENDGRID_API_KEY."'");
-//$sendgrid = new \SendGrid("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"); //Vijay
 $sendgrid = new \SendGrid($Sendgrid);
 
 try {

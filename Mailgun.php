83d8acf03b762752a73dda51b17e459a16e1e7e3
VIJAI <vijay@stepnstones.in>
2020-10-08 10:41:09 +0530

    mail test new3222

--- Mailgun.php
+++ Mailgun.php
@@ -3,6 +3,8 @@
 
 # Include the Autoloader (see "Libraries" for install instructions)
 require 'vendor/autoload.php';
+require_once '/var/secure/MailApiKey.php';
+
 use Mailgun\Mailgun;
 # Instantiate the client.
 $mgClient = new Mailgun($Mailgun);
@@ -11,7 +13,7 @@ $domain = "mail.anklesaria.com";
 # Make the call to the client.
 $result = $mgClient->sendMessage($domain, array(
     'from'  => 'New Mail  <sharathreddy@stepnstones.in>',
-    'to'    => 'Play Boy <sharathreddy@stepnstones.in>',
+    'to'    => 'Test Mail <sharathreddy@stepnstones.in>',
     'subject' => 'Hello',
     'text'  => 'Final Testing some Mailgun awesomness!'
 ));

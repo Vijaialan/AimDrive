f774da29a7c5006d7bcf87c08b6cb9a0f32cd612
VIJAI <vijay@stepnstones.in>
2020-10-11 12:10:46 +0530

    mail content

--- mail_content.php
+++ mail_content.php
@@ -1,94 +1,54 @@
-<style>
-  * {
-    box-sizing: border-box;
-  }
-
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    width: 60%;
-  }
-
-  /* Style the header */
-  header {
-    background-color: #6bb3f7;
-    padding: 10px;
-    text-align: center;
-    color: white;
-  }
-
-  section {
-    width: 90%;
-    text-align: center;
-    margin: auto;
-  }
-
-  /* Style the footer */
-  footer {
-    background-color: #6bb3f7;
-    padding: 10px;
-    text-align: center;
-    color: white;
-  }
-
-  tr {
-    line-height: 25px;
-  }
-
-  #subject {
-    text-align: center;
-  }
-
-  /* Responsive layout - makes the two columns/boxes stack on top of each other instead of next to each other, on small screens */
-  @media (max-width: 600px) {
-
-    nav,
-    article {
-      width: 100%;
-      height: auto;
-    }
-  }
-</style>
 <?php
-
 
 //  $body = CreateHtml('test proj','testing');
 //  echo $body;
+
 function CreateHtml($Content)
 {
   $htmlbody = '
 <html lang="en">
   <head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"></head>
   <body><header><img src="images/logo_big.png"></header><section><table>
-  <h3 id="subject">You have been added to the Project ' . $Content['ProjectName'] . '</h3>
-      
-                    <tr>
-                        <td style="width: 30%;"><p>Project name : </p></td> 
-                        <td style="width: 70%;"><p>' . $Content['ProjectName'] . '</p></td> 
-                    </tr>
-  
-                      <tr>
-                        <td style="width: 30%;"><p>Project Description : </p></td> 
-                        <td style="width: 70%;"><p>' . $Content['TaskDescription'] . '</p></td> 
-                    </tr>
-  
-                    <tr>
-                        <td style="width: 30%;"><p>Supplier Name : </p></td> 
-                        <td ><p>' . $Content['TaskDescription'] . '</p></td> 
-                    </tr>
-  
-                     <tr>
-                        <td style="width: 30%;"><p>Project Value : </p></td> 
-                        <td style="width: 70%;"><p>' . $Content['TaskDescription'] . '</p></td> 
-                    </tr> 
-                     <tr>
-                        <td style="width: 30%;"><p>Start Date : </p></td> 
-                        <td style="width: 70%;"><p> ' . $Content['TaskDescription']. '</p></td> 
-                    </tr> 
+  <h3 id="subject">' . $Content[''] . '</h3>';
+
+  if(!empty($Content['ProjectName']))
+  {
+    $Pname = '<tr  style ="visibility:hidden";">
+              <td style="width: 30%;"><p>Project name : </p></td> 
+              <td style="width: 70%;"><p>' . $Content['ProjectName'] . '</p></td> 
+              </tr>';
+  }
+  if(!empty($Content['TaskDescription']))
+  {
+    $TaskDesc = '<tr>
+                <td style="width: 30%;"><p>Task Description : </p></td> 
+                <td style="width: 70%;"><p>' . $Content['TaskDescription'] . '</p></td> 
+                </tr>';
+  }
+  if(!empty($Content['DueDate']))
+  {
+    $DueDate = '<tr>
+                <td style="width: 30%;"><p>Due Date  : </p></td> 
+                <td ><p>' . $Content['DueDate'] . '</p></td> 
+               </tr>';
+  }
+  if(!empty($Content['ProcessStep']))
+  {
+    
+    $ProcessStep = '<tr>
+                    <td style="width: 30%;"><p>Process Step : </p></td> 
+                    <td style="width: 70%;"><p>' . $Content['ProcessStep'] . '</p></td> 
+                    </tr>'; 
+  }
+              
+  $bodyContent = $Pname + $TaskDesc + $DueDate + $ProcessStep;
                       
-    </table></section>
+  $footer = '</table></section>
   <footer><p>Copyright © 2020-2021 Aim&Drive, All Rights Reserved.</p></footer></body></html>';
 
-  return $htmlbody;
+
+
+  return $htmlbody + $bodyContent + $footer;
 }
 
 ?>
